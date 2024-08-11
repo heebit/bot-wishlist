feb0dafb006f666dd9297a7e7759c79c138cdfe0
heebit <heebit66@yandex.ru>
2024-08-11 12:44:04 +0500

    изменения

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-const { Telegraf } = require('telegraf')
+const { Telegraf } = require('telegraf');
 const fs = require('fs');
-const { message } = require('telegraf/filters')
+const { message } = require('telegraf/filters');
 require('dotenv').config();
 
 const { BOT_TOKEN } = process.env;
@@ -11,47 +11,49 @@ const ADMIN_ID = Number(process.env.ADMIN_ID);
 
 const wishlist = JSON.parse(fs.readFileSync('wishlist.json', 'utf-8'));
 
-bot.start((ctx) => ctx.reply('В этот бот можно добавить свой вишлист. \n Спикок команд \n /wishlist - список подарков \n /add - добавить подарок (только для админа) \n /delete - удалить подарок (только для админа) \n /buy - выбрать подарок '))
+bot.start((ctx) =>
+  ctx.reply(
+    'В этот бот можно добавить свой вишлист. \n Спикок команд \n /wishlist - список подарков \n /add - добавить подарок (только для админа) \n /delete - удалить подарок (только для админа) \n /buy - выбрать подарок ',
+  ),
+);
 
 bot.command('wishlist', (ctx) => {
-    const gifts = wishlist.map((gift, index) => {
-        const bought = gift.boughtBy
-            ? `(куплено ${gift.boughtBy})`
-            : `(никем не куплено)`
-        const row = `${index + 1}. ${gift.title} ${bought}`
-        return row
-    })
-    ctx.reply(`Список подарков: \n${gifts.join('\n')}`)
-})
+  const gifts = wishlist.map((gift, index) => {
+    const bought = gift.boughtBy ? `(куплено ${gift.boughtBy})` : `(никем не куплено)`;
+    const row = `${index + 1}. ${gift.title} ${bought}`;
+    return row;
+  });
+  ctx.reply(`Список подарков: \n${gifts.join('\n')}`);
+});
 bot.command('add', (ctx) => {
-    if (ctx.from.id !== ADMIN_ID) {
-        ctx.reply("Вы не можете добавлять подарок");
-        return;
-    }
-    const giftTitle = ctx.message.text.split(' ').slice(1).join(' ');
-    const gift = { title: giftTitle, boughtBy: null };
-    wishlist.push(gift);
-    fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8',)
-    ctx.reply(`Подарок ${giftTitle} был успешно добавлен`)
-})
+  if (ctx.from.id !== ADMIN_ID) {
+    ctx.reply('Вы не можете добавлять подарок');
+    return;
+  }
+  const giftTitle = ctx.message.text.split(' ').slice(1).join(' ');
+  const gift = { title: giftTitle, boughtBy: null };
+  wishlist.push(gift);
+  fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8');
+  ctx.reply(`Подарок ${giftTitle} был успешно добавлен`);
+});
 bot.command('delete', (ctx) => {
-    if (ctx.from.id !== ADMIN_ID) {
-        ctx.reply("Вы не можете удалить подарок");
-        return;
-    }
-    const giftIndex = Number(ctx.message.text.split(' ')[1])
-    const [delitedGift] = wishlist.splice(giftIndex - 1, 1)
-    fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8',)
-    ctx.reply(`Подарок ${delitedGift.title} успешно удален`)
-})
+  if (ctx.from.id !== ADMIN_ID) {
+    ctx.reply('Вы не можете удалить подарок');
+    return;
+  }
+  const giftIndex = Number(ctx.message.text.split(' ')[1]);
+  const [delitedGift] = wishlist.splice(giftIndex - 1, 1);
+  fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8');
+  ctx.reply(`Подарок ${delitedGift.title} успешно удален`);
+});
 bot.command('buy', (ctx) => {
-    const giftIndex = Number(ctx.message.text.split(' ')[1])
-    wishlist[giftIndex - 1].boughtBy = ctx.from.username;
-    fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8',)
-    ctx.reply(`Подарок ${wishlist[giftIndex - 1].title} будет куплен ${ctx.from.username}`)
-})
-bot.launch()
+  const giftIndex = Number(ctx.message.text.split(' ')[1]);
+  wishlist[giftIndex - 1].boughtBy = ctx.from.username;
+  fs.writeFileSync('wishlist.json', JSON.stringify(wishlist, null, 2), 'utf8');
+  ctx.reply(`Подарок ${wishlist[giftIndex - 1].title} будет куплен ${ctx.from.username}`);
+});
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
